4e0e2551356c55ddd61a797def5a6a8a7a40768a
ksy8888 <ksy8_13@naver.com>
2024-01-10 15:19:34 +0900

    0110_배열 이용한 최소&최댓값과 인덱스 위치 찾기

---
+++ 기초/src/기초부터/배열문제3_10818.java
@@ -0,0 +1,36 @@
+package 기초부터;
+
+import java.io.*;
+import java.util.*;
+
+public class 배열문제3_10818 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		int max = nums[0];
+		int min = nums[0];
+		for(int j=0; j<nums.length; j++) {
+			if(min > nums[j]) {
+				min = nums[j];
+			} else if(max < nums[j]) {
+				max = nums[j];
+			}
+		}
+		sb.append(min).append(" ").append(max);
+		bw.write(String.valueOf(sb));
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
